b17b2b26b8281aeaa414c8a4cb82f92bb95af29b
Jackie <jacgut19@gmail.com>
2018-02-22 17:21:01 +0000

    dynamic route

--- app.js
+++ app.js
@@ -3,18 +3,23 @@
 var express = require("express");
 var app = express();
 
-app.get("/cities", function(request, response){
-  var blocks = ['Providence', 'Cranston', 'Warwick', 'Pawtucket', 'Johnston'];
+var blocks = {
+  'Providence': 'Rhode Island', 
+  'Cranston': 'Rhode Island', 
+  'Warwick': 'Warwick', 
+  'Pawtucket': 'Rhode Island', 
+  'Johnston': 'Rhode Island'};
+
+app.get("/cities/:states", function(request, response){
+  //var blocks = ['Providence', 'Cranston', 'Warwick', 'Pawtucket', 'Johnston'];
+  var description = blocks[request.params.name];
+  response.json(description);
   
   if (request.query.limit >= 0){
         response.json(blocks.slice(0, request.query.limit));
     } else{
     response.json(blocks);
     }
-  // Create a /cities route in your app.js file with at least 4 cities.
-  //var blocks = '<ul><li>Providence</li><li>Cranston</li><li>Pawtucket</li><li>Warwick</li></ul>';
-  //response.sendFile(__dirname + "/public/index.html");
-  // Create an index route that refers to the following file requirement.
 }); 
 
 app.use(express.static('public'));
