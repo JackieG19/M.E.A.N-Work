17b0049de8f7e09b9fe3d219bbba4983737f3d0d
Jackie <jacgut19@gmail.com>
2018-02-27 03:19:21 +0000

    mongoDB

---
+++ MongoDB/mongo.js
@@ -0,0 +1,9 @@
+{
+  "_id" : ObjectId("5a94270a1d84468fbbba8b04"),
+  "name": "Fire Bender",
+  "creator": "Fox",
+  "level_required": 10,
+  "attacks": ["burn", "crack", "blaze"],
+  "attack": 5,
+  "defense": 4
+}
